3b0966ecb27f710f0cf5652a87dcc2618fe9d673
siddhant1309 <siddhantpathak13@gmail.com>
2024-03-22 17:36:32 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const path = require("path");
+const collection = require("./config");
+const bcrypt = require('bcrypt');
+
+const app = express();
+// convert data into json format
+app.use(express.json());
+// Static file
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: false }));
+//use EJS as the view engine
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
+
+// Register User
+app.post("/signup", async (req, res) => {
+
+    const data = {
+        name: req.body.username,
+        password: req.body.password
+    }
+
+    // Check if the username already exists in the database
+    const existingUser = await collection.findOne({ name: data.name });
+
+    if (existingUser) {
+        res.send('User already exists. Please choose a different username.');
+    } else {
+        // Hash the password using bcrypt
+        const saltRounds = 10; // Number of salt rounds for bcrypt
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+
+        data.password = hashedPassword; // Replace the original password with the hashed one
+
+        const userdata = await collection.insertMany(data);
+        console.log(userdata);
+    }
+
+});
+
+// Login user 
+app.post("/login", async (req, res) => {
+    try {
+        const check = await collection.findOne({ name: req.body.username });
+        if (!check) {
+            res.status(404).send("User not found" );
+            return;
+        }
+
+        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+        if (!isPasswordMatch) {
+            res.status(401).send("Incorrect password" );
+            return;
+        }
+
+        res.render("home");
+    }
+    catch {
+        res.status(500).send( "Server error" );
+    }
+});
+
+
+
+
+// Define Port for Application
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+});
